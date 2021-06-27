5e814fadb959596a3bae9d34a9d4049bb1dd9b24
Dobromir Kotsev <dobromir.kotsev@gmail.com>
2021-06-27 22:59:16 +0300

    add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,16 @@ module.exports = {
         display: "swap",
       },
     },
+    {
+    resolve: `gatsby-plugin-google-gtag`,
+    options: {
+      // You can add multiple tracking ids and a pageview event will be fired for all of them.
+      trackingIds: [
+        "G-3Q2G49663Y"]
+    }
+    },
+
+
 
     {
       resolve: `gatsby-plugin-manifest`,
